1d839dd1c85f21d7605438433203d2c6b56d0760
morinlandon12 <morinlandon@gmail.com>
2023-07-30 19:06:41 -0400

    changes

--- lab_4/lab4/build_push.sh
+++ lab_4/lab4/build_push.sh
@@ -5,12 +5,12 @@ IMAGE_PREFIX=$(az account list --all | jq '.[].user.name' | grep -i berkeley.edu
 IMAGE_NAME=lab4
 ACR_DOMAIN=w255mids.azurecr.io
 COMMIT_HASH=$(git rev-parse --short HEAD)
-IMAGE_FQDN="${ACR_DOMAIN}/${IMAGE_PREFIX}/${IMAGE_NAME}:${COMMIT_HASH}"
+IMAGE_FQDN="$ACR_DOMAIN/$IMAGE_PREFIX/$IMAGE_NAME"
 
 az acr login --name w255mids
 
-docker build --platform linux/amd64 -t ${IMAGE_NAME}:${COMMIT_HASH} .
+docker build --platform linux/amd64 -t $IMAGE_NAME:$COMMIT_HASH .
 
-docker tag ${IMAGE_NAME} ${IMAGE_FQDN}
-docker push ${IMAGE_FQDN}
-docker pull ${IMAGE_FQDN}
+docker tag $IMAGE_NAME:$COMMIT_HASH $IMAGE_FQDN
+docker push $IMAGE_FQDN
+docker pull $IMAGE_FQDN
